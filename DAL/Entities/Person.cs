1e924d170b585ad284c0b7902c6f2a739a5af61a
Sergey <Sergey@DESKTOP-M290EKA>
2023-02-16 15:04:51 +0300

    Добавьте файлы проекта.

---
+++ DAL/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Person
+    {
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Patronymic { get; set; } = null!;
+        public string Sex { get; set; } = null!;
+        public DateTimeOffset BirthDate { get; set; }
+    }
+}
